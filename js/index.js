ec200e649566e0b42f9da2021aa4f7f0b415e028
CM-2022011015\user1 <vivianpoiu@gmail.com>
2022-01-20 08:59:11 +0800

    test page

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 let data = [],
-    currentCity='',
-    currentDistrict='';
+  currentCity = '',
+  currentDistrict = '';
 
 const url = 'https://data.coa.gov.tw/Service/OpenData/ODwsv/ODwsvTravelFood.aspx',
-      elementCity = document.querySelector('#City'),
-      elementDistrict = document.querySelector('#District'),
-      elementFood = document.querySelector('#Food'); 
+  elementCity = document.querySelector('#City'),
+  elementDistrict = document.querySelector('#District'),
+  elementFood = document.querySelector('#Food');
 
-const init = async() => {
+const init = async () => {
   await getData();
   renderData();
-  setFilter();
+  setEvent();
 }
 
 const getData = async () => {
@@ -18,87 +18,113 @@ const getData = async () => {
     const res = await fetch(url);
     data = await res.json();
   }
-  catch(err) {
+  catch (err) {
     console.log(err);
   }
 }
 
 const renderData = () => {
-  renderFood();
-  renderDropdowns('City');
+  elementFood.innerHTML = setFood();
+  elementCity.innerHTML = makeDropdownHtml('City');
   document.querySelector('#Loading').classList.add('js-hidden');
 }
 
-const renderFood = (city, district) => {
-  let str='';
-  data.map( (item) => {
-    if(city!==undefined && item.City !== city || district!==undefined && district !== item.Town){
-      return;      
-    } 
-    else {
-      return str+=`
-        <li class="food__item food__item-transition">
-          ${item?.Url && `<a href=${item.Url} target="_blank">`}
-            <div class="food__desc">
-              <h5 class="food__location">${item.Town}</h5>
-              <h4 class="food__restaurant">${item.Name}</h4>
-              <div class="food__underline"></div>
-              <p class="food__details">${item.FoodFeature.substring(0, 43)}...</p>
-            </div>
-            <h5 class="food__tag">${item.City}</h5>
-            <div class="food__imgContainer">
-              <img class="food__img img-resp" src=${item.PicURL} alt=${item.Name} loading="lazy"/>
-            </div>
-            <div class="food__filter"></div>
-          ${item.Url && `</a>`}
-          </li>`;
-      }
-    });
-  elementFood.innerHTML = str;
+const setFood = (arr = []) => {
+  arr = filterFood();
+  return makeFoodHtml(arr);
 }
 
-const getDropdowns = ( keyword, arr) => {
-  keyword === 'City' ?
-  data.forEach(item => {
-    if(!arr.includes(item.City)){
-      arr.push(item.City);
-    }
-  }) 
-  :data.forEach(item => {
-    if( currentCity === item.City) {
-      if(!arr.includes(item.Town)) {
-        arr.push(item.Town);
+const filterFood = (arr = []) => {
+  if (currentCity && currentDistrict) {
+    data.map(item => {
+      if (item.Town === currentDistrict) {
+        arr.push(item);
       }
-    }
-  })
+    })
+  }
+  else if (currentCity) {
+    data.map(item => {
+      if (item.City === currentCity) {
+        arr.push(item);
+      }
+    })
+  }
+  else {
+    arr = data;
+  }
   return arr;
 }
 
-const renderDropdowns = (keyword) => {
-  let arr = [];
+const makeFoodHtml = (arr) => {
+  let str = '';
+  arr.map((item) => {
+    str += `
+      <li class="food__item">
+        <div class="food__gutter">
+          ${item?.Url && `<a href="${item.Url}" target="_blank">`}
+            <div class="food__desc">
+              <span class="food__location">${item.Town}</span>
+              <h2 class="food__restaurant">${item.Name}</h2>
+              <p class="food__details">${item.FoodFeature.substring(0, 43)}...</p>
+            </div>
+            <span class="food__tag">${item.City}</span>
+            <div class="food__imgContainer">
+              <div class="food__img" 
+                style="background-image: linear-gradient(to top,rgba( 0, 0, 0, .5), rgba( 0, 0, 0, .1)), url(${item.PicURL})">
+                ${item.Name}
+              </div>
+            </div>
+          ${item.Url && `</a>`}
+        <div>
+      </li>`;
+  });
+  return str;
+}
+
+const getDropdowns = (keyword, arr) => {
+  keyword === 'City'
+    ? data.forEach(item => {
+      if (!arr.includes(item.City)) {
+        arr.push(item.City);
+      }
+    })
+    : data.forEach(item => {
+      if (currentCity === item.City) {
+        if (!arr.includes(item.Town)) {
+          arr.push(item.Town);
+        }
+      }
+    })
+  return arr;
+}
+
+const makeDropdownHtml = (keyword, arr = []) => {
   arr = getDropdowns(keyword, arr);
-  let renderedData = keyword === 'City'? elementCity.innerHTML:'<option class="filter__dropdown-item" value="" id="default-option" selected disabled>請選擇鄉鎮區...</option>';
+  let dropdownStr = keyword === 'City'
+    ? elementCity.innerHTML
+    : '<option class="filter__dropdownItem" value="" id="default-option" selected disabled>請選擇鄉鎮區...</option>';
   arr.map(item => {
-    renderedData+=`<option class="filter__dropdown-item" id="dropdown-item" value=${item}>${item}</option>`;
+    dropdownStr += `<option class="filter__dropdownItem" value="${item}">${item}</option>`;
   })
-  if(keyword==='City') {
-    elementCity.innerHTML = renderedData;
+  return dropdownStr;
+}
+
+const setEvent = () => {
+  elementCity.addEventListener('change', setDropdowns);
+  elementDistrict.addEventListener('change', setDropdowns);
+}
+
+const setDropdowns = (e) => {
+  if (e.target.id === 'City') {
+    currentCity = elementCity.value;
+    currentDistrict = '';
+    elementFood.innerHTML = setFood();
+    elementDistrict.innerHTML = makeDropdownHtml('Town');
   }
   else {
-    elementDistrict.innerHTML = renderedData;
-  }
-} 
-
-const setFilter = () => {
-  elementCity.addEventListener('change', () => {
-    currentCity = elementCity.value;
-    renderFood(currentCity);
-    renderDropdowns('Town');
-  });
-  elementDistrict.addEventListener('change', () => {
     currentDistrict = elementDistrict.value;
-    renderFood(currentCity,currentDistrict);
-  });
+    elementFood.innerHTML = setFood();
+  }
 }
 
 init();
